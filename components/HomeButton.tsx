51dfd22285eb75acbbbac696745be297d3d364a9
Ivo Berger <ivo@ivoberger.com>
2020-04-19 12:23:19 +0200

    fine tune home button

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -8,9 +8,9 @@ const HomeButton = () => (
     <a className="px-5 py-2 transition duration-500 border rounded-full bg-gray-100-t border-gray-100-t group-hover:border-gray-800">
       <span className="font-sans text-gray-700 transition-all duration-500 group-hover:text-black">
         <FontAwesomeIcon
-          className="transition-all duration-500 group-hover:-ml-1 group-hover:mr-1"
+          className="mr-1 transition-all duration-300 group-hover:-ml-1 group-hover:mr-2"
           icon={faArrowLeft}
-        />{" "}
+        />
         Home
       </span>
     </a>
