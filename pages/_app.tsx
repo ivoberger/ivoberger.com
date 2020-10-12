4568b7deaa2b609731ec7b9bd6dbba4a5805f592
Ivo Berger <ivo@ivoberger.com>
2020-10-12 13:21:08 +0200

    feat: add plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 
 import "../css/index.css";
 import "typeface-catamaran";
@@ -8,10 +9,10 @@ import { Footer } from "components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <PlausibleProvider domain="ivoberger.com">
       <Component {...pageProps} />
       <Footer />
-    </>
+    </PlausibleProvider>
   );
 }
 
