e76900f84b8634a65595a624ef641b219961b0e0
Ivo Berger <ivo.berger@main-incubator.com>
2021-03-26 18:42:58 +0100

    chore: improve eslint config to not throw errors in cjs files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,16 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	extends: ['eslint:recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['.eslintrc.js'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{ files: ['*.svelte'], processor: 'svelte3/svelte3' },
+		// For *.cjs, set env to node to remove the require() complaints.
+		{ files: ['*.cjs'], env: { node: true } },
+		// Enable the TS rules here instead, so the *.cjs override doesn't inherit them.
+		{ files: ['!*.cjs'], extends: 'plugin:@typescript-eslint/recommended' }
+	],
 	settings: {
 		'svelte3/typescript': require('typescript')
 	},
