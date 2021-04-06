8dbd7519c976597c6e4324e2aafb5f240a394563
Ivo <16694197+ivoberger@users.noreply.github.com>
2021-04-06 15:16:18 +0200

    refactor: clean up deps, update config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,20 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'prettier'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['.eslintrc.js'],
-	overrides: [
-		{ files: ['*.svelte'], processor: 'svelte3/svelte3' },
-		// For *.cjs, set env to node to remove the require() complaints.
-		{ files: ['*.cjs'], env: { node: true } },
-		// Enable the TS rules here instead, so the *.cjs override doesn't inherit them.
-		{ files: ['!*.cjs'], extends: 'plugin:@typescript-eslint/recommended' }
-	],
+	ignorePatterns: ['*.cjs'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2018
+		ecmaVersion: 2019
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
 	}
 };
