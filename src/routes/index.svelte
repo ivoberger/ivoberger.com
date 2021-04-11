b4d0cc912b8d962505c614a9d707b8e3e3ec67d7
Ivo <16694197+ivoberger@users.noreply.github.com>
2021-04-11 16:01:26 +0200

    feat: cleaner typography

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -55,6 +55,6 @@
 		@apply mb-2 text-xs uppercase;
 	}
 	.description {
-		@apply px-2 text-lg leading-normal sm:px-4 md:px-10 mt-8;
+		@apply px-2 text-lg leading-normal sm:px-4 md:px-10 mt-6;
 	}
 </style>
