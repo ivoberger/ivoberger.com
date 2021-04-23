2d6f9d80196e9ff6fc500a0c1ad51d133f7b02cf
Ivo Berger <ivo.berger@main-incubator.com>
2021-04-23 19:18:52 +0200

    fix: SEO on article pages

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -6,12 +6,12 @@ import { rootUrl } from '$lib/seoConstants';
 const makeSitemap = (pages: string[], posts: PostSpec[]) => `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     <url><loc>https://${rootUrl}</loc></url>
-    ${pages.map((page) => `<url><loc>https://${rootUrl}/${page}/</loc></url>`).join('')}
+    ${pages.map((page) => `<url><loc>https://${rootUrl}/${page}</loc></url>`).join('')}
     ${posts
 			.map(
 				({ meta: { slug, date } }) => `
     <url>
-        <loc>https://${rootUrl}/posts/${slug}/</loc>
+        <loc>https://${rootUrl}/posts/${slug}</loc>
         <lastmod>${date}</lastmod>
     </url>`
 			)
