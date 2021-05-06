92df8a425f633d582a127667ced3870219624038
Ivo Berger <ivo.berger@main-incubator.com>
2021-05-06 22:59:28 +0200

    feat: 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Body from '$lib/components/Body.svelte';
+</script>
+
+<svelte:head>
+	<title>Ooops, there's no page here</title>
+	<script>
+		plausible('404', { props: { path: document.location.pathname } });
+	</script>
+</svelte:head>
+
+<Body class="prose dark:prose-light text-center">
+	<h1>404</h1>
+	<h1>This page doesn't exist</h1>
+	<p class="text-xl">Here's a cool image of a black hole though.</p>
+	<div class="image-blurred" />
+	<p>PS.: It might have sucked in the page you're looking for...</p>
+</Body>
+
+<style>
+	.image-blurred {
+		background-image: url('404.jpg');
+		width: 100%;
+		height: 25rem;
+		@apply bg-center bg-cover bg-no-repeat;
+		box-shadow: 0 0 15px 10px white inset;
+
+		@media (prefers-color-scheme: dark) {
+			box-shadow: 0 0 15px 10px #171717 inset;
+		}
+	}
+</style>
