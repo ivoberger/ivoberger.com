bbaf987d8b80aeb2b01cbc1367f2ac0b4fa28676
Ivo <16694197+ivoberger@users.noreply.github.com>
2021-05-14 00:13:56 +0200

    merge

--- src/routes/feed.xml.ts
+++ src/routes/feed.xml.ts
@@ -3,7 +3,25 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { getAllPosts, getPost } from '$lib/posts';
 import { defaultDesc, defaultTitle, rootUrl } from '$lib/seoConstants';
 
-const makeFeed = (posts: PostSpec[]) => `<?xml version="1.0" encoding="UTF-8" ?>
+const makeFeed = async (posts: PostSpec[]) => {
+	const postsData: string = (
+		await Promise.all(
+			posts.map(
+				async ({ meta: { slug, date, title, description, tags } }) => `
+<item>
+<title>${title}</title>
+<description>${description}</description>
+<content><![CDATA[${(await getPost(slug)).content}]]></content>
+${tags.map((tag) => `<category>${tag}</category>`).join('\n')}
+<link>https://${rootUrl}/posts/${slug}</link>
+<guid>https://${rootUrl}/posts/${slug}</guid>
+<pubDate>${new Date(date).toUTCString()}</pubDate>		
+</item>`
+			)
+		)
+	).join('');
+
+	return `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" >
 <channel>
 	<title>${defaultTitle}</title>
@@ -11,26 +29,14 @@ const makeFeed = (posts: PostSpec[]) => `<?xml version="1.0" encoding="UTF-8" ?>
 	<atom:link href="https://${rootUrl}/feed.xml" rel="self" type="application/rss+xml" />
 	<description>${defaultDesc}</description>
 	<language>en-us</language>
-    ${posts
-			.map(
-				({ meta: { slug, date, title, description, tags } }) => `
-    <item>
-		<title>${title}</title>
-		<description>${description}</description>
-		<content>${getPost(slug).content}</content>
-		${tags.map((tag) => `<category>${tag}</category>`).join('\n')}
-        <link>https://${rootUrl}/posts/${slug}</link>
-        <guid>https://${rootUrl}/posts/${slug}</guid>
-        <pubDate>${new Date(date).toUTCString()}</pubDate>		
-    </item>`
-			)
-			.join('')}
+    ${postsData}
 </channel>
 </rss>`;
+};
 
-export const get: RequestHandler = () => {
+export const get: RequestHandler = async () => {
 	const posts = getAllPosts();
-	const feed = makeFeed(posts);
+	const feed = await makeFeed(posts);
 
 	return {
 		headers: { 'Content-Type': 'application/rss+xml' },
--- src/routes/index.json.ts
+++ src/routes/index.json.ts
@@ -1,10 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
-import { compareDesc } from 'date-fns';
 
 import { getAllPosts } from '$lib/posts';
 
 export const get: RequestHandler = () => ({
-	body: getAllPosts()
-		.map(({ meta }) => meta)
-		.sort(({ date: dateA }, { date: dateB }) => compareDesc(new Date(dateA), new Date(dateB)))
+	body: getAllPosts().map(({ meta }) => meta)
 });
