f301dc61184baeeeeefbefcbec7ef928e1b09778
Ivo <16694197+ivoberger@users.noreply.github.com>
2021-05-22 18:02:29 +0200

    feat: self host fonts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,6 +11,8 @@
 
 <script>
 	import 'virtual:windi.css';
+	import '@fontsource/catamaran/variable.css';
+	import '@fontsource/source-serif-pro/400.css';
 	import { Footer } from '$lib/components';
 </script>
 
@@ -28,7 +30,7 @@
 	h4,
 	h5,
 	h6 {
-		@apply font-serif;
+		@apply font-serif tracking-tight;
 	}
 
 	h1 {
